97bd06070bc9114efb5ea735952f245a4ce2dc79
Muthuraj D <muthurajwork@zohomail.in>
2026-09-02 18:14:13 +0530

    Initial Commit

---
+++ psso-client-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  psso-client-swiftui
+//
+//  Created by test on 02/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ psso-client-swiftui/psso_client_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  psso_client_swiftuiApp.swift
+//  psso-client-swiftui
+//
+//  Created by test on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct psso_client_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
